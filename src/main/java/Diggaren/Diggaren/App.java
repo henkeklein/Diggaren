8911b7045c362770cf403464fcb16d254a94693d
henkeklein <henrikklein@hotmail.com>
2015-09-30 15:23:20 +0200

    Första push

---
+++ src/main/java/Diggaren/Diggaren/App.java
@@ -0,0 +1,15 @@
+package Diggaren.Diggaren;
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	get("/kanaler", (req, res) -> "Hej");
+        System.out.println( "Hello World!" );
+    }
+}
