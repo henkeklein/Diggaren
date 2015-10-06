6643979e46cfa924411e3ba5988ad6e1d4685dc8
danielhertzman-ericson <daniel.hertzman12@gmail.com>
2015-10-06 09:38:10 +0200

    Rensning av kod

--- src/main/java/Diggaren/Beans/Spotify/SpotifyBean.java
+++ src/main/java/Diggaren/Beans/Spotify/SpotifyBean.java
@@ -6,26 +6,20 @@ import java.util.TreeMap;
 import com.google.gson.annotations.SerializedName;
 
 public class SpotifyBean {
-	
 
 	@SerializedName("artists")
-//	public Map<String, ArtistBean> artists = new TreeMap<String, ArtistBean>();
+	// public Map<String, ArtistBean> artists = new TreeMap<String,
+	// ArtistBean>();
 
 	public ArtistBean artists = new ArtistBean();
 
-	
 	// @SerializedName("track")
 	// private TrackBean beanInfo;
 
-
-		/**
-		 * Being a bean, the class needs an empty, public constructor.
-		 */
-		public SpotifyBean() {
-			
-		}
-
-
+	/**
+	 * Being a bean, the class needs an empty, public constructor.
+	 */
+	public SpotifyBean() {}
 
 	// public TrackBean getBeanInfo() {
 	// return beanInfo;
@@ -39,8 +33,5 @@ public class SpotifyBean {
 
 	public ArtistBean getPlaylist() {
 		return artists;
-		}
 	}
-
-
-
+}
