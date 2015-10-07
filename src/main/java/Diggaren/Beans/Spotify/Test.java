1310e196308c7b2c9c81c49abf8ab48f9a534762
danielhertzman-ericson <daniel.hertzman12@gmail.com>
2015-10-07 13:03:03 +0200

    Kan nu skriva ut från items. Gjort en separat klass för detta

--- src/main/java/Diggaren/Beans/Spotify/Test.java
+++ src/main/java/Diggaren/Beans/Spotify/Test.java
@@ -21,6 +21,7 @@ import org.apache.http.impl.client.HttpClients;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,7 +35,7 @@ import org.json.JSONObject;
  * @author
  */
 public class Test {
-
+	
 	/**
 	 * The main method that's used to run the program.
 	 * 
@@ -43,8 +44,8 @@ public class Test {
 	 *            at all.
 	 */
 	public static void main(String[] args) {
-		String artist = JOptionPane.showInputDialog("Name an artist");
-		String baseUrl = "https://api.spotify.com/v1/search?query="+artist+"&offset=0&limit=1&type=artist";
+//		String artist = JOptionPane.showInputDialog("Name an artist");
+		String baseUrl = "https://api.spotify.com/v1/search?query=Queen&offset=0&limit=1&type=artist";
 
 		HttpClient httpclient = null;
 		HttpGet httpGet = null;
@@ -83,11 +84,10 @@ public class Test {
 //					}
 					
 					
-					JSONObject jObj = readJsonFromUrl(baseUrl);
-					System.out.println(jObj);
 					
 					
-			//		printSong(envelope.artists);
+					
+					printSong(envelope.artists);
 
 				} catch (Exception e) {
 					// Something didn't went well. No calls for us.
@@ -110,31 +110,6 @@ public class Test {
 
 	}
 	
-	
-	public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-	    InputStream is = new URL(url).openStream();
-	    try {
-	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-	      String jsonText = readAll(rd);
-	      JSONObject json = new JSONObject(jsonText);
-	      return json;
-	    } finally {
-	      is.close();
-	    }
-	  }
-	
-	private static String readAll(Reader rd) throws IOException {
-	    StringBuilder sb = new StringBuilder();
-	    int cp;
-	    while ((cp = rd.read()) != -1) {
-	      sb.append((char) cp);
-	    }
-	    return sb.toString();
-	  }
-	
-	private static String readID(Reader rd) throws IOException {
-		return null;
-	}
 }
 
 
